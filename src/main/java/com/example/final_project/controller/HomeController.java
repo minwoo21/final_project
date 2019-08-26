a4aa1844abb6a6dada90fad11d1f60fb1bc04cca
minwoo21 <alsdnrla21@gmail.com>
2019-08-26 15:32:54 +0900

    commit

--- src/main/java/com/example/final_project/controller/HomeController.java
+++ src/main/java/com/example/final_project/controller/HomeController.java
@@ -1,23 +1,14 @@
-package com.example.dog_chain.controller;
-
-import java.util.Map;
+package com.example.final_project.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-
-	@RequestMapping(value = "/home/{action}")
-	public String actionMethod(@RequestParam Map<String, Object> paramMap, @PathVariable String action, ModelAndView modelandView) {
-      
-        return "/home/" + action;
-	
-	}
+    @RequestMapping(value = {"/home", "/thymeleafmerge/thymeleafExtend.html"})
+    public void action(){
+    }
 }
