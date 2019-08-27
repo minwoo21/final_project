36b6033a6042f9f8abbfaf1faf7a684ffc5e4c17
JaewoongByeon <jwbyeon330@gmail.com>
2019-08-27 09:30:29 +0900

    html

--- src/main/java/com/example/final_project/controller/HomeController.java
+++ src/main/java/com/example/final_project/controller/HomeController.java
@@ -8,7 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class HomeController {
-    @RequestMapping(value = {"/index", "/thymeleafmerge/thymeleafExtend.html"})
-    public void action(){
+    @RequestMapping(value = { "/index", "/thymeleafmerge/thymeleafExtend.html" })
+    public void action() {
+    }
+
+    @RequestMapping(value = { "/login/*", "/layout/*" })
+    public void test() {
     }
 }
