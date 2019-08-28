348031588b17405bb52e2f7ce787453682f4983e
JaewoongByeon <jwbyeon330@gmail.com>
2019-08-28 11:42:42 +0900

    service

--- src/main/java/com/example/final_project/repository/MemberRepository.java
+++ src/main/java/com/example/final_project/repository/MemberRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LoginRepository extends JpaRepository<ClassBean, String> {
+public interface MemberRepository extends JpaRepository<ClassBean, String> {
 
 }
